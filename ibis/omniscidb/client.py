6204a0a0d8f02bbf1595907c5c24e34b0ba1e077
anmyachev <45976948+anmyachev@users.noreply.github.com>
2020-03-30 23:52:09 +0300

    add check before close pymapd's connection (#11)

--- ibis/omniscidb/client.py
+++ ibis/omniscidb/client.py
@@ -607,6 +607,7 @@ class OmniSciDBClient(SQLClient):
         self.db_name = database
         self.protocol = protocol
         self.session_id = session_id
+        self.con = None
 
         self._check_execution_type(ipc=ipc, gpu_device=gpu_device)
 
@@ -660,7 +661,8 @@ class OmniSciDBClient(SQLClient):
 
     def close(self):
         """Close OmniSciDB connection and drop any temporary objects."""
-        self.con.close()
+        if self.con and not self.con.closed:
+            self.con.close()
 
     def _adapt_types(self, descr):
         names = []
